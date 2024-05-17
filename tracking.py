4724e5933e94695ae7f9e44a309ea887fec212cc
ram <35802333+Sriram-Sai-Ganesh@users.noreply.github.com>
2024-05-17 11:13:58 -0400

    save progress before final changes

--- tracking.py
+++ tracking.py
@@ -2,11 +2,13 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
 
-ARUCO_DICT = aruco.getPredefinedDictionary(aruco.DICT_4X4_250)
 from utils import rad_to_deg
 
+from aruco_classes import ArucoBall, ArucoVision
 
-import math
+ARUCO_DICT = aruco.getPredefinedDictionary(aruco.DICT_4X4_250)
+MARKER_SIZE = 0.02
+BALL_PCKL_FILEPATH = 'calib/from_video/arupickle.pkl'
 
 
 def track(cap, matrix_coefficients, distortion_coefficients):
@@ -14,8 +16,16 @@ def track(cap, matrix_coefficients, distortion_coefficients):
     parameters = cv2.aruco.DetectorParameters()
     detector = cv2.aruco.ArucoDetector(ARUCO_DICT, parameters)
     
+    # objects wrapping frame parsing and orientation calculations
+    vis = ArucoVision()
+    ball = ArucoBall()
+    ball.load(BALL_PCKL_FILEPATH)
+    
     while True:
         _, frame = cap.read()
+        
+        
+        marker_orientations = vis.get_marker_orientations(frame)
         
         # change frame to grayscale
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
@@ -29,14 +39,7 @@ def track(cap, matrix_coefficients, distortion_coefficients):
             # iterate over each marker
             for i in range(0, len(ids)):  
                 # Estimate pose of each marker and return the values rvec and tvec -- different from camera coefficients
-                rvec, tvec, _ = my_estimatePoseSingleMarkers(corners[i], 0.02, matrix_coefficients, distortion_coefficients)
-                # print('\t'.join(str(x) for x in rvec))
-                print(type(rvec))
-                angles = rad_to_deg(rvec)
-                print(f'MARKER {ids[i]}')
-                print(f'type of angles is {type(angles)}')
-                print(f'type of first element in angles is {type(angles[0])}')
-                print(f'orientation of marker {ids[i]}:',angles)
+                rvec, tvec, _ = my_estimatePoseSingleMarkers([corners[i]], matrix_coefficients, distortion_coefficients)
                 
                 # Draw a square around the markers
                 aruco.drawDetectedMarkers(frame, corners)  
@@ -54,14 +57,15 @@ def track(cap, matrix_coefficients, distortion_coefficients):
         if key == ord('q'):  # Quit
             break
         if np.all(ids is not None):
-            print('\n\n')
+            pass
+        
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
 
 
 # https://stackoverflow.com/questions/75750177
-def my_estimatePoseSingleMarkers(self, corner_locations):
+def my_estimatePoseSingleMarkers(corner_locations, camera_matrix, distortion_coeffs):
     '''
     This will estimate the rvec and tvec for each of the marker corners detected by:
     corners, ids, rejectedImgPoints = detector.detectMarkers(image)
@@ -72,16 +76,16 @@ def my_estimatePoseSingleMarkers(self, corner_locations):
     RETURN list of rvecs, tvecs, and trash (so that it corresponds to the old estimatePoseSingleMarkers())
     '''
     
-    marker_points = np.array([[-self.marker_size / 2, self.marker_size / 2, 0],
-                                [self.marker_size / 2, self.marker_size / 2, 0],
-                                [self.marker_size / 2, -self.marker_size / 2, 0],
-                                [-self.marker_size / 2, -self.marker_size / 2, 0]], dtype=np.float32)
+    marker_points = np.array([[-MARKER_SIZE / 2, MARKER_SIZE / 2, 0],
+                                [MARKER_SIZE / 2, MARKER_SIZE / 2, 0],
+                                [MARKER_SIZE / 2, -MARKER_SIZE / 2, 0],
+                                [-MARKER_SIZE / 2, -MARKER_SIZE / 2, 0]], dtype=np.float32)
     trash = []
     rvecs = []
     tvecs = []
     
     for c in corner_locations:
-        nada, R, t = cv2.solvePnP(marker_points, c, self.camera_matrix, self.dist_coeffs, False, cv2.SOLVEPNP_IPPE_SQUARE)
+        nada, R, t = cv2.solvePnP(marker_points, c, camera_matrix, distortion_coeffs, False, cv2.SOLVEPNP_IPPE_SQUARE)
         rvecs.append(R)
         tvecs.append(t)
         trash.append(nada)
